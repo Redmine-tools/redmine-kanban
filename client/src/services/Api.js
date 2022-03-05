59446e622b9c917cc336ff6071b581e1d6b4d1bc
Sándor Irlanda <sandor.irlanda@gmail.com>
2022-03-05 10:20:55 +0100

    baseURL from .env

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default () => {
     return axios.create({
         // cors proxy url
-        baseURL: 'http://localhost:3000/api'
+        baseURL: process.env.VUE_APP_BACKEND_URL || 'http://localhost:3000/api'
     })
 }
