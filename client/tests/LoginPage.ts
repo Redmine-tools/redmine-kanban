5b712ebc2e16ad7dfe1a9132bc0baa07d24131a7
Bodor Lili <bodor.lili@DEV.TIGRA.HU>
2022-06-17 17:00:58 +0200

    login with api key

--- client/tests/LoginPage.ts
+++ client/tests/LoginPage.ts
@@ -7,6 +7,7 @@ export class LoginPage {
     readonly apiKeyInput;
     readonly loginButton;
     readonly setupTitle;
+    readonly logoutButton;
 
     constructor(readonly page) {
         this.usernameInput = page.locator('[aria-label="Username"]');
@@ -15,15 +16,25 @@ export class LoginPage {
         this.apiKeyInput = page.locator('[aria-label="api key"]');
         this.loginButton = page.locator('button:has-text("Login")');
         this.setupTitle = page.locator('h1:has-text("Kanban board configuration")');
+        this.logoutButton = page.locator('button:has-text("Logout")');
     }
 
     async goto() {
         await this.page.goto('localhost:8080');
     }
 
+    async logout(button) {
+        await this.logoutButton.click(button);
+    }
+
     async loginUsernamePassword(username, password, button) {
         await this.usernameInput.fill(username);
         await this.passwordInput.fill(password);
+        await this.loginButton.click(button);
+    }
+
+    async loginApiKey(apikey, button) {
+        await this.apiKeyInput.fill(apikey);
         await this.loginButton.click(button);
     }
 
--- client/tests/smoke-1.spec.ts
+++ client/tests/smoke-1.spec.ts
@@ -5,11 +5,25 @@ test.beforeEach(async ({ page }) => {
   await new LoginPage(page).goto();
 });
 
+test.afterEach(async ({ page }) => {
+  await new LoginPage(page).logout('Logout');
+});
+
 test('should login with username and password', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   //Fill the usename and password inputs and press the button
   await loginPage.loginUsernamePassword('kobor.levente', 'bookMonkeyBeerShirtFood9', 'Login');
+
+  //Make sure the user successfully loged in
+  await loginPage.expectLogin('Kanban board configuration');
+});
+
+test('should login with api key', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  //Fill the api key input with the api key
+  await loginPage.loginApiKey('975aad20b8ee01cda1527d3daa53074594572365', 'Login');
 
   //Make sure the user successfully loged in
   await loginPage.expectLogin('Kanban board configuration');
